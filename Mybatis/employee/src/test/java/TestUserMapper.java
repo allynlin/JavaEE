ba6648119e1d89c02ffd314ae60c707f81efd203
allynlin <allynlin@outlook.com>
2022-03-16 08:31:50 +0800

    2022-03-16

--- Mybatis/employee/src/test/java/TestUserMapper.java
+++ Mybatis/employee/src/test/java/TestUserMapper.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Random;
 
 public class TestUserMapper {
     @Test
@@ -22,11 +23,17 @@ public class TestUserMapper {
 //        利用SqlSession对象生成一个已实现的接口对象
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
 //        按ID查询
-        User user = mapper.selectById(3);
+//        int min = 1; // 定义随机数的最小值
+//        int max = 3; // 定义随机数的最大值
+        // 产生一个2~100的数
+        int s = 1+(int) (Math.random() * 3);
+        System.out.println(s);
+        User user = mapper.selectById(s);
         System.out.println(user);
 //        关闭会话对象
         sqlSession.close();
     }
+
     @Test
     public void test02() throws IOException {
         //        获取核心配置文件流
@@ -42,15 +49,16 @@ public class TestUserMapper {
         user1.setName("李好好");
         user1.setAge(22);
         user1.setPosition("经理");
-        int n=mapper.add(user1);
+        int n = mapper.add(user1);
         if (n != 0) {
             System.out.println("成功");
         }
         sqlSession.commit();//提交事务
         sqlSession.close();
     }
+
     @Test
-    public void test03() throws IOException{
+    public void test03() throws IOException {
         //        获取核心配置文件流
         InputStream in = Resources.getResourceAsStream("mybatis-config.xml");
 //        构建会话工厂
@@ -64,15 +72,16 @@ public class TestUserMapper {
         user.setAge(30);
         user.setName("刘好好");
         user.setPosition("员工");
-        int n=mapper.update(user);
+        int n = mapper.update(user);
         if (n != 0) {
             System.out.println("成功");
         }
         sqlSession.commit();//提交事务
         sqlSession.close();
     }
+
     @Test
-    public void test04() throws IOException{
+    public void test04() throws IOException {
         //        获取核心配置文件流
         InputStream in = Resources.getResourceAsStream("mybatis-config.xml");
 //        构建会话工厂
@@ -85,7 +94,7 @@ public class TestUserMapper {
 //        user.setUid(5);
 //        user.setUage(30);
 //        user.setUname("刘好好");
-        int n=mapper.deleteById(5);
+        int n = mapper.deleteById(5);
         if (n != 0) {
             System.out.println("成功");
         }
