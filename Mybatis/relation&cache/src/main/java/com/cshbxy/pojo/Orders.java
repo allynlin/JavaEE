d6908fb1c997d67a120bf1ff40367f59c10e3e67
allynlin <allynlin@outlook.com>
2022-03-29 15:40:16 +0800

    2022-03-29

---
+++ Mybatis/relation&cache/src/main/java/com/cshbxy/pojo/Orders.java
@@ -0,0 +1,30 @@
+package com.cshbxy.pojo;
+
+public class Orders {
+    private int id;
+    private String number;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "Orders{" +
+                "id=" + id +
+                ", number='" + number + '\'' +
+                '}';
+    }
+}
--- Mybatis/relation&cache/src/test/java/MyBatisTest.java
+++ Mybatis/relation&cache/src/test/java/MyBatisTest.java
@@ -1,4 +1,5 @@
 import com.cshbxy.pojo.Person;
+import com.cshbxy.pojo.Users;
 import com.cshbxy.untils.MyBatisUtils;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
@@ -12,6 +13,28 @@ public class MyBatisTest {
         Person person = session.selectOne("com.cshbxy.mapper.PersonMapper.findPersonById", 1);
         //输出查询结果信息
         System.out.println(person);
+        //关闭SqlSession
+        session.close();
+    }
+    @Test
+    public void findPersonByIdTest2() {
+        //通过工具箱获取SqlSession对象
+        SqlSession session = MyBatisUtils.getSession();
+        //使用MyBtis嵌套查询的方式查询id为1的人的信息
+        Person person = session.selectOne("com.cshbxy.mapper.PersonMapper.findPersonById2", 1);
+        //输出查询结果信息
+        System.out.println(person);
+        //关闭SqlSession
+        session.close();
+    }
+    @Test
+    public void findUserTest(){
+        //通过工具箱获取SqlSession对象
+        SqlSession session = MyBatisUtils.getSession();
+        //查询id为1的人的信息
+        Users users = session.selectOne("com.cshbxy.mapper.UsersMapper.findUserWithOrders", 1);
+        //输出查询结果信息
+        System.out.println(users);
         //关闭SqlSession
         session.close();
     }
