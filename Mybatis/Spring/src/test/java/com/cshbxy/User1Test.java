c8df93313c6ac99117a320377c304791a4229ba0
allynlin <allynlin@outlook.com>
2022-04-02 23:01:56 +0800

    2022-04-02

--- Mybatis/Spring/src/test/java/com/cshbxy/User1Test.java
+++ Mybatis/Spring/src/test/java/com/cshbxy/User1Test.java
@@ -5,7 +5,7 @@ import org.springframework.context.ApplicationContext;
 public class User1Test {
     public static void main(String[] args) {
         //加载applicationContext.xml配置
-        ApplicationContext context = new org.springframework.context.support.ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext context = new org.springframework.context.support.ClassPathXmlApplicationContext("applicationContext-User1.xml");
         //获取配置中User1对象
         User1 user1 = (User1) context.getBean("user1",User1.class);
         //输出User1对象
--- Mybatis/Spring/src/test/java/com/cshbxy/User2Test.java
+++ Mybatis/Spring/src/test/java/com/cshbxy/User2Test.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class User2Test {
     public static void main(String[] args) {
         //加载applicationContext.xml配置
-        ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext-User2.xml");
         //获取bean
         User2 user2 = ac.getBean("user2", User2.class);
         //输出user2的信息
